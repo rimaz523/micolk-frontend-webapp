bc7af2ebf99cfe7b44c4790bc6f0ad538d2c4630
Mohommed Rimaz <rimazmohommed523@gmail.com>
2021-03-09 21:22:04 +1300

    fix yarn timeout & add Storybook Page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import './App.css'
-import Button from '@material-ui/core/Button'
+import { Page } from './stories/Page'
 
 function App() {
-    return (
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-    )
+    return <Page></Page>
 }
 
 export default App
