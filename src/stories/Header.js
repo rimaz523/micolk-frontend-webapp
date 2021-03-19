eed42cb09b85645dc2ff5fa2e6285e81e19c0ae5
Mohommed Rimaz <rimazmohommed523@gmail.com>
2021-03-19 22:39:54 +1300

    update the page content

--- src/stories/Header.js
+++ src/stories/Header.js
@@ -29,7 +29,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
                         />
                     </g>
                 </svg>
-                <h1>Acme</h1>
+                <h1>MicoLK</h1>
             </div>
             <div>
                 {user ? (
