1433847dbc81502967033e25f6cc8a55a34a1d4b
Mohommed Rimaz <rimazmohommed523@gmail.com>
2021-07-17 15:39:57 +1200

    refactor existing react components (#20)

---
+++ src/stories/components/footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './footer.css'
+
+export const Footer = ({ preContent, postContent }) => (
+    <div className="footer">
+        <p>
+            {preContent}
+            {new Date().getFullYear()}
+            {postContent}
+        </p>
+    </div>
+)
+
+Footer.propTypes = {
+    preContent: PropTypes.string,
+    postContent: PropTypes.string,
+}
+
+Footer.defaultProps = {
+    preContent: '© ',
+    postContent: ' Mate Inc. All Rights Reserved',
+}
