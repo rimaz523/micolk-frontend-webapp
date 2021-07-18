2d72301d7f5a6d20e0b7dd6d5e0bf010a0ec9ff8
Mohommed Rimaz <rimazmohommed523@gmail.com>
2021-07-18 14:15:33 +1200

    use material UI and recreate header/footer components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React from 'react'
 import './App.css'
 import { Page } from './stories/Page'
 
+import { createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
 function App() {
-    return <Page></Page>
+    const prefersDarkMode = true
+
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode]
+    )
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Page></Page>
+        </ThemeProvider>
+    )
 }
 
 export default App
--- src/stories/components/footer/Footer.js
+++ src/stories/components/footer/Footer.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './footer.css'
+import { Container } from '@material-ui/core'
+import { Box } from '@material-ui/core'
 
-export const Footer = ({ preContent, postContent }) => (
-    <div className="mc-footer">
-        <p>
-            {preContent}
-            {new Date().getFullYear()}
-            {postContent}
-        </p>
-    </div>
-)
+export const Footer = ({ preContent, postContent }) => {
+    return (
+        <footer>
+            <Box borderTop={1}>
+                <Container>
+                    <Box
+                        textAlign="center"
+                        pt={{ xs: 2, sm: 5 }}
+                        pb={{ xs: 2, sm: 5 }}
+                    >
+                        {preContent}
+                        {new Date().getFullYear()}
+                        {postContent}
+                    </Box>
+                </Container>
+            </Box>
+        </footer>
+    )
+}
 
 Footer.propTypes = {
     preContent: PropTypes.string,
